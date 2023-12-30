fbca6b309e5941e778085907a0587712383a7108
bak3839 <bak3839@naver.com>
2023-12-30 16:51:06 +0900

    Learn about example domain design and the benefits of querydsl

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,83 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+    @Autowired
+    EntityManager em;
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void testEntity() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamA);
+        Member member3 = new Member("member3", 10, teamB);
+        Member member4 = new Member("member4", 10, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush();
+        em.clear();
+
+        List<Member> resultList = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : resultList) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team" + member.getTeam());
+        }
+    }
+
+    @Test
+    public void startJPQL() {
+        //member1을 찾아라
+        String qlString =
+                "select m from Member m " +
+                "where m.username = :username";
+        Member result = em.createQuery(qlString, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(result.getUsername()).isEqualTo("member1");
+    }
+
+    //querydsl은 파라메터 바인딩이 없다(자동으로 해줌)
+    @Test
+    public void startQuerydsl() {
+        QMember m = new QMember("m");
+
+        Member result = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(result.getUsername()).isEqualTo("member1");
+    }
+}
